0deb8902ba48f56ad71d94a43a4e7a887e4af13d
dolisha <dolishagandhi@gmail.com>
2026-04-17 23:37:29 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ let app = express();
 
 app.use(cors({
   
-  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN ,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
